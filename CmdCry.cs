d506b0669f42df463a74e4a3d608ff4c1fb079fe
capitjeff21 <36058298+capitjeff21@users.noreply.github.com>
2020-10-26 09:34:24 -0400

    Add files via upload

---
+++ CmdCry.cs
@@ -0,0 +1,17 @@
+using System;
+using MCGalaxy;
+
+namespace MCGalaxy.Commands.Chatting {
+	public sealed class CmdCry : MessageCmd {
+			public override string name { get { return "High5"; } }
+			
+			public override void Use(Player p, string message, CommandData data) {
+				TryMessageAction(p, message, "λNICK %Sis crying because of λTARGET", true);
+			}
+
+			public override void Help(Player p) {
+				p.Message("%T/Cry <player> - cry, optionally because of <player>");
+			}
+		}
+	}
+
---
+++ CmdDrink.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MCGalaxy;
+using System.Collections.Generic;
+namespace MCGalaxy.Commands.Chatting{
+    public sealed class CmdDrink : MessageCmd
+    {
+        public override string name { get { return "Drink"; } }
+        public override string type { get { return "other"; } }
+
+        public override void Use(Player p, string message, CommandData data)
+        {
+          var drinkMessages = new List<string>() { " drank lemonade", " drank water", " drank milk", " drank soda" };
+          var random = new Random();
+        int index = random.Next(drinkMessages.Count);
+        Chat.MessageGlobal(p.name + drinkMessages[index]);
+        }
+
+        public override void Help(Player p) {
+            p.Message("%T/Drink %H- Drinks a random drink.");
+        }
+    }
+}
