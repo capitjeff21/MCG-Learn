979db7f8afe4b7d0e26e452914a8e9ac6b24948b
capitjeff21 <36058298+capitjeff21@users.noreply.github.com>
2020-10-27 09:56:20 -0400

    Update CmdCry.cs

--- CmdCry.cs
+++ CmdCry.cs
@@ -6,8 +6,12 @@ namespace MCGalaxy.Commands.Chatting {
 			public override string name { get { return "Cry"; } }
 			
 			public override void Use(Player p, string message, CommandData data) {
-				TryMessageAction(p, message, "λNICK %Sis crying because of λTARGET", true);
-			}
+				Player target = PlayerInfo.FindMatches(p, name);
+				if (target == null) {
+					Chat.MessageGlobal(p.name + " is crying");
+				} else{
+				Chat.MessageGlobal(p.name + " is crying because of " + target.name);
+			}}
 
 			public override void Help(Player p) {
 				p.Message("%T/Cry <player> - cry, optionally because of <player>");
